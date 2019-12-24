49f291489703dafd90fc975b40277381f6e549ef
KUAN-HSUN-LI <b06209027@ntu.edu.tw>
2019-12-24 23:21:10 +0800

    clean TA comment

--- args.py
+++ args.py
@@ -1,10 +1,8 @@
 import argparse
-from datetime import datetime
 
 
 def get_args():
-    # TODO: Add --start-date, --end-date and --output arguments
-    #       Convert the two dates to datetime objects
+    """ Implentation arguments of crawler including start-date, end_date and output"""
     parser = argparse.ArgumentParser()
     parser.add_argument("--start-date", default="2019-01-01", help="format: [Year-Month-Day] ex:2019-01-01")
     parser.add_argument("--end-date", default="2019-12-31", help="format: [Year-Month-Day] ex:2019-12-31")
